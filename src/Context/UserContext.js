ac3f4980002ff48e609e2728b4620450fd324ea9
rajibdas-it <rajibrad@gmail.com>
2022-11-09 12:41:14 +0600

    userCreate and login function create on userContext paaf

--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -1,16 +1,29 @@
-import React, { createContext } from "react";
-import { getAuth } from "firebase/auth";
+import React, { createContext, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import app from "../Firebase/firebase.config";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const UserContext = ({ children }) => {
-  const user = {
-    userName: "Rajib",
-    email: "rajibrad@gmail.com",
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
   };
-  const authInfo = { user };
+
+  const userLogin = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const authInfo = { userLogin };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
